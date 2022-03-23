8cf86d04d3ca3a6ca36a6f5c9559066362d5a64d
Yacine TAZDAIT <yacine.tazdait@epita.fr>
2022-03-23 15:53:56 +0100

    Correction de la partie 4 : v1
    création de 2 instances de Serveur et 2 instances de Client pour pouvoir respectivement recevoir et émettre simultanément au lancement du launcher

--- src/main/java/fr/lernejo/navy_battle/Launcher.java
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -9,9 +9,15 @@ import java.net.http.HttpResponse;
 
 public class Launcher {
     public static void main(String[] args) throws IOException, InterruptedException {
-        Server server = new Server(9876, "localhost");
-        server.init();
-        Client client = new Client(8765);
-        HttpResponse resp = client.sendRequest(8765, "http://localhost:9876");
+        Server s1 = new Server(9876, "localhost");
+        s1.init();
+        Server s2 = new Server(8765, "localhost");
+        s2.init();
+        Client c1 = new Client(9876);
+        HttpResponse rep1 = c1.sendRequest(9876, "http://localhost:8765");
+        System.out.println("Reponse de s2 :" + rep1);
+        Client c2 = new Client(8765);
+        HttpResponse rep2 = c2.sendRequest(8765, "http://localhost:9876");
+        System.out.println("Reponse de s1 :" + rep2);
     }
 }
