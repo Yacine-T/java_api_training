69bb0449020a261f81d1fa432a29e2e2ba33e8ff
Yacine TAZDAIT <yacine.tazdait@epita.fr>
2022-03-26 13:08:43 +0100

    correction de la testsuite - partie 2,3 et 4 : v1

--- src/test/java/fr/lernejo/server/StartHttpHandlerTest.java
+++ src/test/java/fr/lernejo/server/StartHttpHandlerTest.java
@@ -11,6 +11,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StartHttpHandlerTest {
 
+    Runtime rt = Runtime.getRuntime();
 
     @Test
     public void testStartHttphHandler_WhenHttpMethodIsValidAndBody_ShouldReturnCodeStatus202() throws IOException, InterruptedException {
@@ -23,20 +24,19 @@ class StartHttpHandlerTest {
 
     @Test
     public void testStartHttphHandler_WhenHttpMethodIsValidButBodyIsntValid_ShouldReturnCodeStatus400() throws IOException, InterruptedException {
-        Server server = new Server(9876, "localhost");
+        Server server = new Server(5000, "localhost");
         server.init();
-
         Client client = new Client(8795);
-        HttpResponse resp =  client.sendWrongPostRequest(8765, "http://localhost:9876/api/game/start");
+        HttpResponse resp =  client.sendWrongPostRequest(8765, "http://localhost:5000/api/game/start");
         Assertions.assertEquals(400, resp.statusCode());
     }
 
     @Test
     public void testStartHttphHandler_WhenHttpMethodIsntValid_ShouldReturnCodeStatus404() throws IOException, InterruptedException {
-        Server server = new Server(9876, "localhost");
+        Server server = new Server(5001, "localhost");
         server.init();
         Client client = new Client(8795);
-        HttpResponse resp =  client.sendGetRequest(8765, "http://localhost:9876/api/game/start");
+        HttpResponse resp =  client.sendGetRequest(8765, "http://localhost:5001/api/game/start");
         Assertions.assertEquals(404, resp.statusCode());
     }
 
