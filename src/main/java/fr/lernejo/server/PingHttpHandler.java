229598e58e37b416122258840959f0b9e0738dbd
Yacine TAZDAIT <yacine.tazdait@epita.fr>
2022-03-26 15:26:49 +0100

    correction des TU

--- src/main/java/fr/lernejo/server/PingHttpHandler.java
+++ src/main/java/fr/lernejo/server/PingHttpHandler.java
@@ -10,7 +10,7 @@ public class PingHttpHandler implements HttpHandler {
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        String body = "Hello";
+        String body = "OK";
         exchange.sendResponseHeaders(200, body.length());
         try (OutputStream os = exchange.getResponseBody()) { // (1)
             os.write(body.getBytes());
--- src/test/java/fr/lernejo/server/StartHttpHandlerTest.java
+++ src/test/java/fr/lernejo/server/StartHttpHandlerTest.java
@@ -17,8 +17,8 @@ class StartHttpHandlerTest {
     public void testStartHttphHandler_WhenHttpMethodIsValidAndBody_ShouldReturnCodeStatus202() throws IOException, InterruptedException {
         Server server = new Server(9875, "localhost");
         server.init();
-        Client client = new Client(8795);
-        HttpResponse resp =  client.sendPostRequest(8765, "http://localhost:9875/api/game/start");
+        Client client = new Client(9875);
+        HttpResponse resp =  client.sendPostRequest(8764, "http://localhost:9875/api/game/start");
         Assertions.assertEquals(202, resp.statusCode());
     }
 
@@ -26,8 +26,8 @@ class StartHttpHandlerTest {
     public void testStartHttphHandler_WhenHttpMethodIsValidButBodyIsntValid_ShouldReturnCodeStatus400() throws IOException, InterruptedException {
         Server server = new Server(5000, "localhost");
         server.init();
-        Client client = new Client(8795);
-        HttpResponse resp =  client.sendWrongPostRequest(8765, "http://localhost:5000/api/game/start");
+        Client client = new Client(5000);
+        HttpResponse resp =  client.sendWrongPostRequest(8763, "http://localhost:5000/api/game/start");
         Assertions.assertEquals(400, resp.statusCode());
     }
 
@@ -35,8 +35,8 @@ class StartHttpHandlerTest {
     public void testStartHttphHandler_WhenHttpMethodIsntValid_ShouldReturnCodeStatus404() throws IOException, InterruptedException {
         Server server = new Server(5001, "localhost");
         server.init();
-        Client client = new Client(8795);
-        HttpResponse resp =  client.sendGetRequest(8765, "http://localhost:5001/api/game/start");
+        Client client = new Client(5001);
+        HttpResponse resp =  client.sendGetRequest(8762, "http://localhost:5001/api/game/start");
         Assertions.assertEquals(404, resp.statusCode());
     }
 
